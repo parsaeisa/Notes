74e67e14ae954e91a537bdfca5964c2d2af3b94a
parsaeisa <parsaeisa@github.com>
2023-05-25 20:02:16 +0330

    uncomment commands

--- Software engineering/Database/Database class/Seeds/students.sql
+++ Software engineering/Database/Database class/Seeds/students.sql
@@ -1,7 +1,7 @@
--- insert into students (id, full_name, major) values (1, 'Ruthi', 'Recruiting Manager');
--- insert into students (id, full_name, major) values (2, 'Reina', 'Research Nurse');
--- insert into students (id, full_name, major) values (3, 'Amitie', 'Help Desk Technician');
--- insert into students (id, full_name, major) values (4, 'Charil', 'Design Engineer');
--- insert into students (id, full_name, major) values (4, 'Charil', 'Design Engineer');
--- insert into students (id, full_name, major) values (5, 'Esdras', 'Help Desk Operator');
+insert into students (id, full_name, major) values (1, 'Ruthi', 'Recruiting Manager');
+insert into students (id, full_name, major) values (2, 'Reina', 'Research Nurse');
+insert into students (id, full_name, major) values (3, 'Amitie', 'Help Desk Technician');
+insert into students (id, full_name, major) values (4, 'Charil', 'Design Engineer');
+insert into students (id, full_name, major) values (4, 'Charil', 'Design Engineer');
+insert into students (id, full_name, major) values (5, 'Esdras', 'Help Desk Operator');
 insert into students (id, full_name, major) values (6, 'Billy', 'Help Desk Operator');
